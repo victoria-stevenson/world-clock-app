9fff58d2fff36d1d26956771163df1a611057e5a
VSWild <134617590+victoria-stevenson@users.noreply.github.com>
2023-06-10 15:55:19 +0200

    Added JS time and date according to different time zones, and updating in real time

---
+++ src/index.js
@@ -0,0 +1,54 @@
+//Stockholm
+setInterval(function () {
+  let stockholmElement = document.querySelector("#stockholm");
+  let stockholmDateElement = stockholmElement.querySelector(".date");
+  let stockholmTimeElement = stockholmElement.querySelector(".time");
+  let stockholmTime = moment.tz("Europe/Stockholm");
+
+  stockholmDateElement.innerHTML = stockholmTime.format("MMMM Do YYYY");
+  stockholmTimeElement.innerHTML = stockholmTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
+
+//Dublin
+setInterval(function () {
+  let dublinElement = document.querySelector("#dublin");
+  let dublinDateElement = dublinElement.querySelector(".date");
+  let dublinTimeElement = dublinElement.querySelector(".time");
+
+  let dublinTime = moment.tz("Europe/Dublin");
+
+  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+  dublinTimeElement.innerHTML = dublinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
+
+//Los Angeles
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
+  let losAngelesTime = moment.tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
+
+//Auckland
+setInterval(function () {
+  let aucklandElement = document.querySelector("#auckland");
+  let aucklandDateElement = aucklandElement.querySelector(".date");
+  let aucklandTimeElement = aucklandElement.querySelector(".time");
+
+  let aucklandTime = moment.tz("Pacific/Auckland");
+
+  aucklandDateElement.innerHTML = aucklandTime.format("MMMM Do YYYY");
+  aucklandTimeElement.innerHTML = aucklandTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
